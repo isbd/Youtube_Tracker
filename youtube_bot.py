7940164d508ef7a9ad2bebd9749d3baa701eef02
Ian Buchanan-Dempsey <buchanandempsia597@gmail.com>
2022-04-26 00:21:58 -0500

    Added pulling for channel id and pulling recent videos fixes

--- youtube_bot.py
+++ youtube_bot.py
@@ -3,6 +3,7 @@ Youtube bot for pulling channel data
 '''
 import configparser
 import json
+from lib2to3.pgen2.literals import test
 from googleapiclient.discovery import build
 
 class YoutubeBot():
@@ -17,14 +18,18 @@ class YoutubeBot():
         '''
         self.token = config['youtube']['api_token']
         self.youtube = build('youtube', 'v3', developerKey=self.token)
+        self.channel_list = ['ethoslab']
 
     def run(self):
         '''
         @brief Main bot functionality
         '''
         print('Pulling data')
-        channel_info = self.pull_channel_stats('ethoslab')
+        channel_info = self.pull_channel_stats(self.channel_list[0])
         print(f'Info on channel: {channel_info}')
+        user_id = self.pull_channel_id(self.channel_list[0])
+        video_list = self.get_video_data(user_id, 30)
+        print(f'Info on videos: {video_list}')
     
     def pull_channel_stats(self, username):
         '''
@@ -41,6 +46,38 @@ class YoutubeBot():
         result = self.handle_request(request)
         if result:
             return result['items'][0]['statistics']
+        return False
+
+    def pull_channel_id(self, username):
+        request = self.youtube.channels().list(
+            part = 'id',
+            forUsername = username
+        )
+        result = self.handle_request(request)
+        if result:
+            return result['items'][0]['id']
+        return False
+
+    def get_video_data(self, channel_id, number_to_search):
+        playlist = self.pull_uploads_playlist(channel_id)
+        if playlist:
+            request = self.youtube.playlistItems().list(
+                part="snippet",
+                playlistId=playlist,
+                maxResults=number_to_search
+            )
+            return self.handle_request(request)
+        return False
+
+    def pull_uploads_playlist(self, channel_id):
+        request = self.youtube.channels().list(
+            part="contentDetails",
+            id=channel_id,
+            fields="items(contentDetails/relatedPlaylists/uploads,id)"
+        )
+        result = self.handle_request(request)
+        if result:
+            return result['items'][0]['contentDetails']['relatedPlaylists']['uploads']
         return False
 
     @staticmethod
